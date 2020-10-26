6ebd6a7cc37d02c01cf10b844595bad096d43e44
Andrea Contestabile <andreacontest@gmail.com>
2020-10-26 17:12:03 +0100

    Stile view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,31 +4,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset("css/app.css")}}">
   <title>Laravel-Boolean</title>
 </head>
 <body>
-  <h1>Lista degli Studenti</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>Cognome</th>
-        <th>Data di nascita</th>
-        <th>Luogo di nascita</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($students as $student)
+  <div class="container">
+    <h1>Lista degli Studenti</h1>
+    <table>
+      <thead>
         <tr>
-          <td>{{$student->id}}</td>
-          <td>{{$student->nome}}</td>
-          <td>{{$student->cognome}}</td>
-          <td>{{$student->data_di_nascita}}</td>
-          <td>{{$student->luogo_di_nascita}}</td>
+          <th class="t-heading">ID</th>
+          <th class="t-heading">Nome</th>
+          <th class="t-heading">Cognome</th>
+          <th class="t-heading">Data di nascita</th>
+          <th class="t-heading">Luogo di nascita</th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        @foreach ($students as $student)
+          <tr>
+            <td>{{$student->id}}</td>
+            <td>{{$student->nome}}</td>
+            <td>{{$student->cognome}}</td>
+            <td>{{$student->data_di_nascita}}</td>
+            <td>{{$student->luogo_di_nascita}}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 </body>
 </html>
